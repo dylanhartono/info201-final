db51fc64dd0c50f99b09e040e9199f3375e76ec6
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-19 20:55:05 -0700

    Added chart

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -9,15 +9,14 @@ tempo_pop_data <- data %>%
 #This chart aims to show the relationship between a song's tempo and its popularity.
 #We want to see whether a song's tempo has correlations to its popoularity.
 
-
 scatter_plot <- ggplot(data = tempo_pop_data) + 
   geom_point(aes(x = tempo, y = popularity, color = genre)) 
 
-
-#This bar chart shows the relationship between a song's tempo and its popularity.
+#The bar chart below shows the relationship between a song's tempo and its popularity.
 #From this bar chart, we can see that each genre has an average level of 
 #popularity and tempo of which most songs in that genre belong to.
 
+#Interpretation : 
 #As we can see, there is not one distinct linear relationship between tempo and popularity.
 #On average, for songs that have a popularity level of above 50, the faster the tempo,
 #the lower the popularity. On the other hand, songs that have a popularity level
