d2fa0ac9c80ab76d536c35ab3b9c4f4a0e555476
hdylan1 <hdylan1@uw.edu>
2020-05-20 14:33:06 -0700

    dataset is called top10

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,3 +1,23 @@
 data <- read.csv("../data/UltimateSpotifyTracks.csv", stringsAsFactors = FALSE)
-length(unique(data$ï..genre))
-names(data)
+library(dplyr)
+get_summary_info <- function(dataset) {
+  list(
+    songs <- nrow(dataset),
+    num_features <- ncol(dataset),
+    most_common_key <- dataset %>% 
+      group_by(key) %>% 
+      summarise(count = n()) %>% 
+      filter(count == max(count)) %>% 
+      pull(key),
+    avg_tempo <- mean(dataset$tempo),
+    avg_dance <- mean(dataset$dance),
+    percent_major <- dataset %>% filter(mode == "Major") %>% nrow() / nrow(dataset),
+    top_artist <- dataset %>% 
+      group_by(artist_name) %>% 
+      summarise(count = n()) %>% 
+      filter(count == max(count)) %>% 
+      pull(count)
+  )
+}
+get_summary_info(data)
+
