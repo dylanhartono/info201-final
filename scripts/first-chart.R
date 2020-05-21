bdcfdf17f3fb322f4d5d17bd9c37d28ed6d8fad5
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-20 23:03:02 -0700

    added first chart to rmd

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -6,8 +6,6 @@ library(plotly)
 library(RColorBrewer)
 
 
-
-
 first_chart <- function(data_df) {
   tempo_pop_data <- data_df %>%
     filter(!is.na(bpm)) %>%
@@ -19,26 +17,3 @@ first_chart <- function(data_df) {
   labs(x = "popularity (100 - most popular)", y = "bpm (beats per minute)")
 )
 }
-
-
-#This chart aims to show the relationship between a song's 
-#tempo and its popularity.
-#We want to see whether a song's tempo has correlations to its popoularity.
-
-#Popularity index was taken from spotify's popularity index.
-#The higher the value, the more popular the song was, with
-# 100 meaning the highest possible level of popularity attainable on Spotify.
-
-
-# Interpretation:
-#From the line plot, we can see that as as the popularity level increases,
-#the bpm decreases, converging at about 115 bpm. This observation changes
-#when popularity level hits above 50, where there is an upward-sloping
-#relationship in which the bpm increases with popularity level.
-#Once we pass a popularity level of 72, we can see that the
-#higher the popularity,the lower the bpm.
-#This can imply that slow songs have been more popular from 2010-2019.
-
-#We also note that there is a large error margin, as there are values
-#with low popularity (eg. 0) with really low bpm and high bpm.
-
