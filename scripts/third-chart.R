472a0ad7b8dd77c22b3f41f8f5c14390c2fddbe5
张巧依 <zhangqiaoyi@zhangqiaoyideMacBook-Pro.local>
2020-05-21 14:15:16 -0700

    new version of third chart

--- scripts/third-chart.R
+++ scripts/third-chart.R
@@ -1,20 +1,41 @@
 library("dplyr")
 library("plotly")
 
-top_10_songs <- read.csv("../data/top10s.csv",stringsAsFactors = FALSE)  
+third_chart <- function(top_songs){
+  top_songs <- top_songs %>%
+    group_by(year) %>%
+    filter(pop == max(pop))
+# chart Most popular genres of the year(2010-2019)
+  x <- ggplot(data=top_songs, aes(x=paste(year, title), y=pop, fill=top.genre)) +
+    geom_bar(stat="identity") +
+    coord_flip() +
+    labs(title = "Most popular pop songs from 2010 to 2019", 
+         x = "Song Title", 
+         y = "Popularity") 
+  fig <- ggplotly(x)
+  fig
+}
+# dataset based on the song with the highest popularity of that year 
 
-top_10_songs <- top_10_songs %>%
+top_songs <- read.csv("../data/top10s.csv",stringsAsFactors = FALSE)  
+
+top_songs <- top_songs %>%
   group_by(year) %>%
   filter(pop == max(pop))
-  
+
 
 # chart Most popular genres of the year(2010-2019)
-p <- ggplot(data=top_10_songs, aes(x=year, y=pop, fill=top.genre)) +
+x <- ggplot(data=top_songs, aes(x=paste(year, title), y=pop, fill=top.genre)) +
   geom_bar(stat="identity") +
-  labs(title = "Most popular genres of the year(2010-2019)", 
-       x = "Year", 
+  coord_flip() +
+  labs(title = "Most popular pop songs from 2010 to 2019", 
+       x = "Song Title", 
        y = "Popularity") 
 
-fig <- ggplotly(p)
-
+fig <- ggplotly(x)
 fig
+
+
+
+
+
