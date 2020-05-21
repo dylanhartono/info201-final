7fbcbf2b31a7c7f5709089a4d4ada9d96e03d931
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-20 20:45:11 -0700

    made changes

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(plotly)
 library(RColorBrewer)
 
-data<- read.csv("../data/top10s.csv", stringsAsFactors = FALSE)
+data <- read.csv("../data/top10s.csv", stringsAsFactors = FALSE)
 
 length(unique(data$title))
 
@@ -19,7 +19,8 @@ tempo_pop_data <- data %>%
 
 
 
-#This chart aims to show the relationship between a song's tempo and its popularity.
+#This chart aims to show the relationship between a song's 
+#tempo and its popularity.
 #We want to see whether a song's tempo has correlations to its popoularity.
 
 #Popularity index was taken from spotify's popularity index.
@@ -27,13 +28,13 @@ tempo_pop_data <- data %>%
 # 100 meaning the highest possible level of popularity attainable on Spotify.
 
 
-# Interpretation: 
+# Interpretation:
 #From the line plot, we can see that as as the popularity level increases,
 #the bpm decreases, converging at about 115 bpm. This observation changes
-#when popularity level hits above 50, where there is an upward-sloping 
+#when popularity level hits above 50, where there is an upward-sloping
 #relationship in which the bpm increases with popularity level.
-#Once we pass a popularity level of 72, we can see that the 
-#higher the popularity,the lower the bpm. 
+#Once we pass a popularity level of 72, we can see that the
+#higher the popularity,the lower the bpm.
 #This can imply that slow songs have been more popular from 2010-2019.
 
 #We also note that there is a large error margin, as there are values
@@ -42,7 +43,7 @@ tempo_pop_data <- data %>%
 
 ggplotly(ggplot(data = tempo_pop_data) +
   geom_smooth(mapping = aes(y = bpm, x = pop)) +
-  labs(x ="popularity (100 - most popular)", y = "bpm (beats per minute)")
+  labs(x = "popularity (100 - most popular)", y = "bpm (beats per minute)")
 )
 
 b <- plot_ly(
@@ -57,13 +58,8 @@ b <- plot_ly(
                 "</br> Tempo: ", bpm,
                 "</br> Popularity: ", pop)
 ) %>%
-  layout (
+  layout(
     title = "Tempo VS. Popularity",
     xaxis = list(title = "bpm (beats per minute"),
     yaxis = list(title = "popularity")
   )
-
-
-  
-
-
