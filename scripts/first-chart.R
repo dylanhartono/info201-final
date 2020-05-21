cb043a12f0846ab5bef637ead6e37341514633cd
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-20 20:40:14 -0700

    made changes

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -7,39 +7,59 @@ library(RColorBrewer)
 
 data<- read.csv("../data/top10s.csv", stringsAsFactors = FALSE)
 
+length(unique(data$title))
+
+#filter out repeated songs and NA values
 tempo_pop_data <- data %>%
   filter(!is.na(bpm)) %>%
-  filter(!is.na(pop))
+  filter(!is.na(pop)) %>%
+  filter(!duplicated(title)) %>%
+  filter(bpm != 0)
+  
 
 
 
 #This chart aims to show the relationship between a song's tempo and its popularity.
 #We want to see whether a song's tempo has correlations to its popoularity.
 
-#The bar chart below shows the relationship between a song's tempo and its popularity.
+#Popularity index was taken from spotify's popularity index.
+#The higher the value, the more popular the song was, with
+# 100 meaning the highest possible level of popularity attainable on Spotify.
 
 
-#measured in 
-#what do the chunks represent
-#scatter 
+# Interpretation: 
+#From the line plot, we can see that as as the popularity level increases,
+#the bpm decreases, converging at about 115 bpm. This observation changes
+#when popularity level hits above 50, where there is an upward-sloping 
+#relationship in which the bpm increases with popularity level.
+#Once we pass a popularity level of 72, we can see that the 
+#higher the popularity,the lower the bpm. 
+#This can imply that slow songs have been more popular from 2010-2019.
+
+#We also note that there is a large error margin, as there are values
+#with low popularity (eg. 0) with really low bpm and high bpm.
+
+
 ggplotly(ggplot(data = tempo_pop_data) +
-  geom_smooth(mapping = aes(x = bpm, y = pop)))
+  geom_smooth(mapping = aes(y = bpm, x = pop)) +
+  labs(x ="popularity (100 - most popular)", y = "bpm (beats per minute)")
+)
 
 b <- plot_ly(
   data = tempo_pop_data,
   x  = ~bpm,
   y = ~pop,
   color = ~top.genre,
-  colors = colorRampPalette(brewer.pal(8,"Dark2"))(27),
+  colors = colorRampPalette(brewer.pal(8, "Dark2"))(27),
   type = "smooth",
-  text = ~paste('</br>', top.genre,
-                ':',
-                '</br> Tempo: ', bpm,
-                '</br> Popularity: ', pop)
+  text = ~paste("</br>", top.genre,
+                ":",
+                "</br> Tempo: ", bpm,
+                "</br> Popularity: ", pop)
 ) %>%
   layout (
     title = "Tempo VS. Popularity",
-    xaxis = list(title = "bpm"),
+    xaxis = list(title = "bpm (beats per minute"),
     yaxis = list(title = "popularity")
   )
 
