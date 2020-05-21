21f3496bda084eb6d580a297d0b7bb05ca04a1b3
张巧依 <zhangqiaoyi@zhangqiaoyideMacBook-Pro.local>
2020-05-21 15:28:14 -0700

    add chart3 and text to Rmd

--- scripts/third-chart.R
+++ scripts/third-chart.R
@@ -1,3 +1,4 @@
+# chart Most popular genres of the year(2010-2019)
 library("dplyr")
 library("plotly")
 
@@ -5,41 +6,12 @@ third_chart <- function(top_songs){
   top_songs <- top_songs %>%
     group_by(year) %>%
     filter(pop == max(pop))
-# chart Most popular genres of the year(2010-2019)
   x <- ggplot(data=top_songs, aes(x=paste(year, title), y=pop, fill=top.genre)) +
     geom_bar(stat="identity") +
     coord_flip() +
     labs(title = "Most popular pop songs from 2010 to 2019", 
-         x = "Song Title", 
+         x = "Year & Song Title", 
          y = "Popularity") 
   fig <- ggplotly(x)
   fig
 }
-# dataset based on the song with the highest popularity of that year 
-
-top_songs <- read.csv("../data/top10s.csv",stringsAsFactors = FALSE)  
-
-top_songs <- top_songs %>%
-  group_by(year) %>%
-  filter(pop == max(pop))
-
-
-# chart Most popular genres of the year(2010-2019)
-x <- ggplot(data=top_songs, aes(x=paste(year, title), y=pop, fill=top.genre)) +
-  geom_bar(stat="identity") +
-  coord_flip() +
-  labs(title = "Most popular pop songs from 2010 to 2019", 
-       x = "Song Title", 
-       y = "Popularity") 
-
-fig <- ggplotly(x)
-fig
-
-
-
-
-
-<<<<<<< HEAD
-=======
-fig
->>>>>>> bdcfdf17f3fb322f4d5d17bd9c37d28ed6d8fad5
