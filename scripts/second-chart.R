f31508a0d8f8f77cc87098e99b5dccb50d8e3fd6
Carol Lei <carollei@Carols-MacBook-Pro.local>
2020-05-22 12:23:31 -0700

    Changes made to second chart

--- scripts/second-chart.R
+++ scripts/second-chart.R
@@ -9,11 +9,13 @@ library(plotly)
 # depicts artist name, track name
 # scatter plot depicting popularity versus danceability
 second_chart <- function(songs_df) {
+  genres <- c(songs_df$top.genre)
   popularity_dance <- ggplotly(ggplot(songs_df) +
     geom_point(mapping = aes(x = pop, y = dnce, color = top.genre)) + 
     scale_fill_brewer(palette = "YlOrRd") + 
-    labs(title = "Danceability versus Popularity", x = "Popularity", y = "Danceability")
-    #scale_fill_discrete(name = "Genre")
+    labs(title = "Danceability versus Popularity", x = "Popularity", y = "Danceability") + 
+    scale_colour_discrete("Genres",
+                          labels = genres)
   )
   return(popularity_dance)
 }
--- scripts/summary-table.R
+++ scripts/summary-table.R
@@ -6,5 +6,6 @@ get_summary_table <- function(dataset) {
       count = n(),
       avg_dnce = round(mean(dnce), digits = 1),
       avg_bpm = round(mean(bpm), digits = 1),
-      )
+      ) %>% 
+    arrange(desc(avg_popularity), desc(count))
 }
