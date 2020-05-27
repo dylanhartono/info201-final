e22b10df6c86abff0614cbc4e697fee8b1aedb21
hdylan1 <hdylan1@uw.edu>
2020-05-27 13:48:29 -0700

    added shiny files

--- scripts/summary-table.R
+++ scripts/summary-table.R
@@ -3,13 +3,13 @@
 # arranged from their average popularity.
 # Digits are rounded to 1 decimal place
 get_summary_table <- function(dataset) {
-  summary_table <- dataset %>% 
-    group_by(top.genre) %>% 
+  summary_table <- dataset %>%
+    group_by(top.genre) %>%
     summarise(
       avg_popularity = round(mean(pop), digits = 1),
       count = n(),
       avg_dnce = round(mean(dnce), digits = 1),
       avg_bpm = round(mean(bpm), digits = 1),
-      ) %>% 
+      ) %>%
     arrange(desc(avg_popularity), desc(count))
 }
--- scripts/summary.R
+++ scripts/summary.R
@@ -7,20 +7,19 @@ get_summary_info <- function(dataset) {
   summary <- list()
   summary$number_of_songs <- nrow(dataset)
   summary$features <- names(
-    dataset %>% 
+    dataset %>%
     select(-title, -artist, -top.genre, -year, -X))
-  summary$avg_pop <- round(mean(dataset$pop), digits=2)
+  summary$avg_pop <- round(mean(dataset$pop), digits = 2)
   summary$genres <- length(unique(data$top.genre))
   summary$top_genre <- dataset %>%
-    group_by(top.genre) %>% 
-    summarise(avg_pop = mean(pop)) %>% 
-    filter(avg_pop == max(avg_pop)) %>% 
+    group_by(top.genre) %>%
+    summarise(avg_pop = mean(pop)) %>%
+    filter(avg_pop == max(avg_pop)) %>%
     pull(top.genre)
-  summary$top_artist <- dataset %>% 
-    group_by(artist) %>% 
-    summarise(count = n()) %>% 
-    filter(count == max(count)) %>% 
+  summary$top_artist <- dataset %>%
+    group_by(artist) %>%
+    summarise(count = n()) %>%
+    filter(count == max(count)) %>%
     pull(artist)
   return(summary)
 }
-
