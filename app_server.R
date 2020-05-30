3e63fb56efcce2d79d8a83c685dfb88c9051b112
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-30 00:51:15 -0700

    first page draft

--- app_server.R
+++ app_server.R
@@ -2,7 +2,15 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
+source("scripts/first-chart.R")
+tempo_data <- data %>%
+  filter(top.genre == input$genre_var)
 
 server <- function(input, output) {
-
+  output$firstchart <- renderPlot({
+    title <- paste("Tempo VS Popularity for: ", input$genre_var)
+    first_chart(tempo_data)
+    })
 }
+
+
--- app_ui.R
+++ app_ui.R
@@ -2,28 +2,27 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-navbarPage(
-  "Title of our project",
-  first_page
-)
-
 genre_list <- unique(select(data, top.genre))
+
 
 first_page <-tabPanel(
   "First chart",
   sidebarLayout(
     sidebarPanel(
       h2("Tempo vs. Popularity"),
-      genre_input <- selectInput(
-        "genre_var",
+      selectInput(
+        inputId = "genre_var",
         label = "Select a genre",
         choices = genre_list,
         selected = ""
       )),
-      mainPanel(
-        #ggplot chart here
-      )
-      
+    mainPanel(
+      plotOutput("firstchart")
     )
   )
+)
 
+ui <- navbarPage(
+  "Title of our project",
+  first_page
+)
