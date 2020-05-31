0e7f5d4cf50057dc5d2f1e93de9f6d913041090c
Dainese Chandra <dainesechandra@MacBook-Air.local>
2020-05-30 20:50:53 -0700

    first page draft

--- app_server.R
+++ app_server.R
@@ -3,12 +3,14 @@ library(ggplot2)
 library(dplyr)
 
 source("scripts/first-chart.R")
+
+choice <<- input$genre_var
 tempo_data <- data %>%
-  filter(top.genre == input$genre_var)
+  filter(top.genre == choice)
 
 server <- function(input, output) {
   output$firstchart <- renderPlot({
-    title <- paste("Tempo VS Popularity for: ", input$genre_var)
+    title <- paste("Tempo VS Popularity for: ", choice)
     first_chart(tempo_data)
     })
 }
